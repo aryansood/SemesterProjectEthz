f9d0b8b12985635bd629ccda202b3691b60ae4a9
Aryan Sood <arsood@eu-a65-06.euler.ethz.ch>
2025-08-06 19:15:47 +0200

    Updating data download utils

---
+++ data_download_utils/training_data_process.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+import os
+import numpy as np
+from waymo_open_dataset.protos import end_to_end_driving_data_pb2 as wod_e2ed_pb2
+import time
+
+DATASET_FOLDER = '/cluster/scratch/arsood/data' #Put the location of where the trfrecords are stored
+
+TRAIN_FILES = os.path.join(DATASET_FOLDER, 'training_*.tfrecord*')
+VALIDATION_FILES = os.path.join(DATASET_FOLDER, 'val*.tfrecord*')
+TEST_FILES = os.path.join(DATASET_FOLDER, 'test_*.tfrecord*')
+
+SAVE_LOCATION = "/cluster/scratch/arsood/data_new" #Put the location of where to save the processed data
+
+CREATE_DIRECTORY = 1
+
+name_folders_train = np.load('training_segments.npy')
+name_folders_val = np.load('val_segments.npy')
+name_folders = name_folders_train #Choose if using name_folders_train or name_folders_val
+for el in name_folders:
+    os.makedirs(os.path.join(SAVE_LOCATION, el), exist_ok=True)
+
+filenames = tf.io.matching_files(VALIDATION_FILES) #Choose beetween TRAIN_FILES, VALIDATION_FILES, TEST_FILES
+dataset = tf.data.TFRecordDataset(filenames, compression_type='')
+dataset_iter = dataset.as_numpy_iterator()
+
+count = 0
+start_time = time.time()
+for bytes_example in dataset_iter:
+    data = wod_e2ed_pb2.E2EDFrame()
+    data.ParseFromString(bytes_example)
+    sequence_name, sample_idx = data.frame.context.name.split('-')
+    out_path = os.path.join(os.path.join(SAVE_LOCATION, sequence_name), f"{data.frame.context.name}.bin")
+    with open(out_path, 'wb') as f:
+        f.write(bytes_example)
+    count = count+1
+    print(count)
+    
+end_time = time.time()
+print(f"Total time taken: {end_time - start_time:.2f} seconds")
+
+
---
+++ model/qwen_model.py
@@ -0,0 +1,56 @@
+import torch
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+from peft import LoraConfig, get_peft_model
+
+class QwenModel():
+    def __init__(self, cache_dir = '/cluster/scratch/arsood/cache_hugging_face', local_files_only= True, model_to_use = 7, device = 'auto'):
+        model_dict = {
+           3: "Qwen/Qwen2.5-VL-3B-Instruct",
+           7: "Qwen/Qwen2.5-VL-7B-Instruct",
+           32: "Qwen/Qwen2.5-VL-32B-Instruct",
+           70: "Qwen/Qwen2.5-VL-70B-Instruct"
+        }
+        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+            model_dict[model_to_use], torch_dtype=torch.float16, device_map=device,
+            cache_dir = cache_dir, local_files_only= local_files_only)
+
+        self.processor = AutoProcessor.from_pretrained(
+            model_dict[model_to_use],
+            cache_dir=cache_dir,
+            local_files_only=local_files_only
+            )
+        
+    def generate(self, messages, images, videos):
+        """
+        messages: List of messages-> Depends on the batch
+        images: List of Images-> Put all the Images in the order the appear
+        videos: List of Videos-> Put all the Videos in the order the appear
+        """
+        self.processor.tokenizer.padding_side = "left"
+        texts = [
+        self.processor.apply_chat_template(msg, tokenize=False, add_generation_prompt=True)
+        for msg in messages
+        ]
+        inputs = self.processor(
+            text=texts,
+            images=images,
+            videos=videos,
+            padding=True,
+            return_tensors="pt",
+        )
+    
+        inputs = inputs.to("cuda")
+        generated_ids = self.model.generate(**inputs, max_new_tokens=100)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+
+        output_text = self.processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+        return output_text
